7903170cd3163c572c16efdc6071bcfec1cd0426
Tom Janssens <Tom@corebvba.be>
2013-09-23 07:50:06 +0200

    Initial commit

---
+++ Propern.Specs/POC.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+
+namespace Propern.Specs
+{
+    [TestClass]
+    public class POC
+    {
+        public class AnInteger : IEnumerable<int>
+        {
+            Random R = new Random();
+            int MinValue;
+            int MaxValue;
+
+            public AnInteger(int MinValue = int.MinValue, int MaxValue = int.MinValue)
+            {
+                this.MinValue = MinValue;
+                this.MaxValue = MaxValue;
+            }
+
+            public IEnumerator<int> GetEnumerator()
+            {
+                while (true)
+                {
+                    yield return R.Next(MinValue, MaxValue);
+                }
+            }
+
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                return this.GetEnumerator();
+            }
+        }
+
+
+
+        [TestMethod]
+        public void Test_a_sum()
+        {
+            Spec.Given(new AnInteger(MinValue: -10, MaxValue: 10)
+                    .Zip(new AnInteger(MinValue: -10, MaxValue: 10), (a, b) => new { a, b }))
+                .When(c => c.a - c.b)
+                .Expect((c, r) => Assert.AreEqual(c.a, r + c.b))
+                .Verify(100);
+        }
+
+        [TestMethod]
+        public void Test_a_div()
+        {
+            Spec.Given(new AnInteger(MinValue: -10, MaxValue: 10)
+                    .Zip(new AnInteger(MinValue: -10, MaxValue: 10).Where(b=> b!=0),(a,b) => new {a,b} ))
+                .When(c => c.a / c.b)
+                .Expect((c, r) => Assert.IsTrue(Math.Abs(r * c.b) <= Math.Abs(c.a)))
+                .Verify(100);
+        }
+
+    }
+}
---
+++ Propern/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace Propern
+{
+
+    public class Spec
+    {
+        public static GivenSpec<T> Given<T>(IEnumerable<T> Givens)
+        {
+            return new GivenSpec<T>(Givens);
+        }
+
+        public class GivenSpec<T>
+        {
+            public readonly IEnumerable<T> Givens;
+            public GivenSpec(IEnumerable<T> Givens)
+            {
+                this.Givens = Givens;
+            }
+
+            public WhenSpec<T, R> When<R>(Func<T, R> Action)
+            {
+                return new WhenSpec<T, R>(Givens, Action);
+            }
+        }
+
+        public class WhenSpec<T, R>
+        {
+            private IEnumerable<T> Givens;
+            private Func<T, R> Action;
+
+            public WhenSpec(IEnumerable<T> Givens, Func<T, R> Action)
+            {
+                this.Givens = Givens;
+                this.Action = Action;
+            }
+
+            public ExpectSpec<T, R> Expect(Action<T, R> Assertion)
+            {
+                return new ExpectSpec<T, R>(Givens, Action, Assertion);
+            }
+        }
+
+        public class ExpectSpec<T, R>
+        {
+            private IEnumerable<T> Givens;
+            private Func<T, R> Action;
+            private Action<T, R> Assertion;
+
+            public ExpectSpec(IEnumerable<T> Givens, Func<T, R> Action, Action<T, R> Assertion)
+            {
+                this.Givens = Givens;
+                this.Action = Action;
+                this.Assertion = Assertion;
+            }
+
+            public void Verify(int Times = 100)
+            {
+                foreach (var context in Givens.Take(Times))
+                {
+                    Debug.WriteLine("Testing context:\n" + JsonConvert.SerializeObject(context));
+                    var r = Action(context);
+                    Debug.WriteLine("Output:\n" + JsonConvert.SerializeObject(r));
+                    Assertion(context, r);
+                    Debug.WriteLine("Asserted===============================");
+                }
+            }
+        }
+    }
+}
